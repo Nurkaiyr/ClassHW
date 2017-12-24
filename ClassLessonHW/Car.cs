9456a796a5498e5ec004ab63276dd1e24c0bce38
Nurkaiyr <yedige21@gmail.com>
2017-12-25 01:07:27 +0600

    Homework

---
+++ ClassLessonHW/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLessonHW
+{
+    public class Car
+    {
+        private string mark;
+        private int year;
+        private string color;
+        private double volume;
+        private string country;
+
+        private static int count;
+
+        public Car()
+        {
+            mark = " ";
+            year = 0;
+            color = " ";
+            volume = 0;
+            country = " ";
+        }
+
+        public Car(string mark, int year, string color, double volume,string country)
+        {
+            this.mark = mark;
+            this.year = year;
+            this.color = color;
+            this.volume = volume;
+            this.country = country;
+        }
+
+        static Car()
+        {
+            count = 0;
+        }
+
+        public string Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public double Volume
+        {
+            get { return volume; }
+            set { volume = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+       
+    }
+}
---
+++ ClassLessonHW/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLessonHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] car = new Car[5];
+            car[0] = new Car("Toyota Camry", 2014, "White", 2.4 , "Japan");
+            car[1] = new Car("Kia", 2015, "Blue", 1.4, "KOREA");
+            car[2] = new Car("Mercedes", 2017, "Black", 2.6, "Germany");
+            car[3] = new Car("BMW", 2015, "Grey", 4.4, "Germany");
+            car[4] = new Car("Lexus", 2012, "White", 3, "Japan");
+
+            for (int i = 0; i < car.Length; i++)
+            {
+                Console.WriteLine("Car["+i+"] : "+ car[i].Mark+" " +car[i].Year+" "+car[i].Color+" "+car[i].Volume+" "+car[i].Country);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
